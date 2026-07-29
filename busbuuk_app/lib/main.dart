bf058960de50d9174651b2d592948e04147cfa6e
frank00067 <f.nkurunziz2@alustudent.com>
2026-07-29 11:29:32 +0200

    added main.dart

---
+++ busbuuk_app/lib/main.dart
@@ -0,0 +1,11 @@
+// entry point - runApp() goes here once the app is built out
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'app.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const BusbuukApp());
+}
